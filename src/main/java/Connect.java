3d6d57f7c0df34b50f262546ee14a10d5fc58ab7
thephez <thephez@users.noreply.github.com>
2021-02-18 11:03:38 -0500

    feat: add platform Connect example

---
+++ src/main/java/Connect.java
@@ -0,0 +1,21 @@
+import org.dashevo.Client;
+import org.dashevo.dapiclient.DapiClient;
+
+public class Connect {
+
+    public static void main(String[] args) {
+        Client sdk = new Client("testnet");
+        sdk.getPlatform().setClient(new DapiClient("174.34.233.134", false));
+        DapiClient client = sdk.getPlatform().client;
+
+        connect(client);
+    }
+
+    private static void connect(DapiClient client) {
+        try {
+            System.out.printf("Connected. Best block hash: %s%n", client.getBestBlockHash());
+        } catch (Exception e) {
+            System.out.printf("\nSomething went wrong:\n%s%n", e.getLocalizedMessage());
+        }
+    }
+}
