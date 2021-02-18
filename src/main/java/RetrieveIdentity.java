acbfa96b4c70f7e6f855efa22b7012874bb9a12e
thephez <thephez@users.noreply.github.com>
2021-02-18 13:00:34 -0500

    feat: add retrieve identity example

---
+++ src/main/java/RetrieveIdentity.java
@@ -0,0 +1,26 @@
+import org.dashevo.Client;
+import org.dashevo.dapiclient.DapiClient;
+import org.dashevo.dpp.identity.Identity;
+import org.json.JSONObject;
+
+public class RetrieveIdentity {
+    static Client sdk;
+    static String id = "BSNxbs99zCFvEtK8qrewYTVxNKt4DnFQ8sPbsb8nrDuf";
+
+    public static void main(String[] args) {
+        sdk = new Client("testnet");
+        // Uncomment the following line to request from a local node running platform services
+        // sdk.getPlatform().setClient(new DapiClient("127.0.0.1", false));
+
+        getIdentity();
+    }
+
+    private static void getIdentity() {
+        try {
+            Identity identity = sdk.getPlatform().getIdentities().get(id);
+            System.out.printf("Retrieved identity: %s%n", new JSONObject(identity != null ? identity.toJSON() : null).toString(2));
+        } catch (Exception e) {
+            System.out.printf("\nSomething went wrong:\n%s%n", e);
+        }
+    }
+}
